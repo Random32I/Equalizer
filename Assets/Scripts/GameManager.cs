6bb83eda0d25a7bb34ae4bba00b18b4bc14c9d86
Random32I <104802424+Random32I@users.noreply.github.com>
2023-11-28 17:27:18 -0500

    Added Title Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,7 +26,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPaused = false;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
